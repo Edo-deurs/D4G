494ded4d69f98fcdf2d75cb42eeca675c6a45830
Destructis <les.3e.anquetil@gmail.com>
2023-11-15 11:48:51 +0100

    adds

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,18 +7,36 @@ xhr.onload = function () {
   if (xhr.status === 200) {
     const data = JSON.parse(xhr.responseText);
     const liste = document.getElementById("maListe");
-    data.criteres.forEach((critere) => {
-      const li = document.createElement("li");
-      const titre = document.createElement("h2");
-      titre.textContent = critere.critere;
-      li.appendChild(titre);
-      const thematique = document.createElement("p");
-      thematique.innerHTML = `<strong>Thématique:</strong> ${critere.thematique}`;
-      li.appendChild(thematique);
-      const objectif = document.createElement("p");
-      objectif.innerHTML = `<strong>Objectif:</strong> ${critere.objectif}`;
-      li.appendChild(objectif);
-      liste.appendChild(li);
+    const select = document.getElementById("filtre");
+    const themes = [
+      ...new Set(data.criteres.map((critere) => critere.thematique)),
+    ];
+    themes.forEach((theme) => {
+      const option = document.createElement("option");
+      option.value = theme;
+      option.textContent = theme;
+      select.appendChild(option);
+    });
+    select.addEventListener("change", function () {
+      const selectedTheme = this.value;
+      while (liste.firstChild) {
+        liste.removeChild(liste.firstChild);
+      }
+      data.criteres
+        .filter((critere) => critere.thematique === selectedTheme)
+        .forEach((critere) => {
+          const li = document.createElement("li");
+          const titre = document.createElement("h2");
+          titre.textContent = critere.critere;
+          li.appendChild(titre);
+          const thematique = document.createElement("p");
+          thematique.innerHTML = `<strong>Thématique:</strong> ${critere.thematique}`;
+          li.appendChild(thematique);
+          const objectif = document.createElement("p");
+          objectif.innerHTML = `<strong>Objectif:</strong> ${critere.objectif}`;
+          li.appendChild(objectif);
+          liste.appendChild(li);
+        });
     });
   } else {
     console.log("Impossible de charger le fichier JSON");
