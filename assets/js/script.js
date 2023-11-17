5979b37652ec58e83f7815ec78a3d8207671e1fc
Destructis <les.3e.anquetil@gmail.com>
2023-11-17 04:21:39 +0100

    plus que du beau et afficher les valeurs
    Co-authored-by: Fallemne <Fallemne@users.noreply.github.com>
    Co-authored-by: Alexquis28 <Alexquis28@users.noreply.github.com>

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,25 +6,7 @@ xhr.open(
 );
 
 
-// Fonction pour enregistrer des données dans localStorage
-function saveTemp(cle, valeur) {
-  console.log("here");
-  tabVal = tabVal || {}; 
-  tabVal[cle] = valeur;
-  localStorage.setItem("etats", JSON.stringify(tabVal));
-}
 
-// Fonction pour récupérer des données depuis localStorage
-function loadLocalStorage(cle) {
-  const valeur = localStorage.getItem("etats");
-  return JSON.parse(valeur);
-}
-const clearLocalStorageBtn = document.getElementById('clear-localstorage');
-
-clearLocalStorageBtn.addEventListener('click', function() {
-  localStorage.clear();
-  alert('localStorage has been cleared!');
-});
 
 xhr.onload = function () {
   if (xhr.status === 200) {
@@ -41,45 +23,74 @@ xhr.onload = function () {
       "non applicable",
       "a definir",
     ];
-    const etats = {};
+    let etats = {};
     let sortDirection = 1;
+    etats = loadLocalStorage() || {};
+    console.log("Initialized etats:", etats);
+    updateCounters();
 
-    function updateCounters() {
-      const conformeCounter = document.getElementById("conforme-counter");
-      const enCoursCounter = document.getElementById("en-cours-counter");
-      const nonConformeCounter = document.getElementById(
-        "non-conforme-counter"
-      );
-      const nonApplicableCounter = document.getElementById(
-        "non-applicable-counter"
-      );
-      const aDefinirCounter = document.getElementById("a-definir-counter");
-      const scoreConformite = document.getElementById("conformity-score");
+    // Fonction pour enregistrer des données dans localStorage
+function saveTemp(cle, valeur) {
+  console.log("here");
+  const loadedStates = loadLocalStorage(); // Chargez les états depuis le localStorage
+  loadedStates[cle] = valeur; // Mettez à jour la valeur
+  localStorage.setItem("etats", JSON.stringify(loadedStates)); // Enregistrez dans le localStorage
+  etats = loadedStates; // Mettez à jour la variable globale etats
+  updateCounters(); // Mettez à jour les compteurs après l'enregistrement
+}
 
-      const etatsCount = Object.values(etats).reduce((count, etat) => {
-        count[etat] = (count[etat] || 0) + 1;
-        return count;
-      }, {});
+// Fonction pour récupérer des données depuis localStorage
+function loadLocalStorage() {
+  const valeur = localStorage.getItem("etats");
+return valeur ? JSON.parse(valeur) : {};
+}
 
-      conformeCounter.textContent = etatsCount["conforme"] || 0;
-      enCoursCounter.textContent = etatsCount["en cours de déploiement"] || 0;
-      nonConformeCounter.textContent = etatsCount["non conforme"] || 0;
-      nonApplicableCounter.textContent = etatsCount["non applicable"] || 0;
-      scoreConformite.textContent = etatsCount["score de conformité"] || 0;
 
-      const totalCritereCount = data.criteres.length;
-      const definedCritereCount = Object.values(etats).filter(
-        (etat) => etat !== "a definir"
-      ).length;
-      const aDefinirCount = totalCritereCount - definedCritereCount;
-      aDefinirCounter.textContent = aDefinirCount;
+const clearLocalStorageBtn = document.getElementById('clear-localstorage');
 
-      const scoreConformiteCount =
-        (etatsCount["conforme"] || 0) /
-        (totalCritereCount - (etatsCount["non applicable"] || 0));
-      scoreConformite.textContent = scoreConformiteCount.toFixed(2) * 100 + "%";
-      return scoreConformite.textContent;
-    }
+clearLocalStorageBtn.addEventListener('click', function() {
+  localStorage.clear();
+  alert('localStorage has been cleared!');
+});
+
+
+function updateCounters() {
+  const conformeCounter = document.getElementById("conforme-counter");
+  const enCoursCounter = document.getElementById("en-cours-counter");
+  const nonConformeCounter = document.getElementById("non-conforme-counter");
+  const nonApplicableCounter = document.getElementById("non-applicable-counter");
+  const aDefinirCounter = document.getElementById("a-definir-counter");
+  const scoreConformite = document.getElementById("conformity-score");
+
+  // Vérifiez si etats est défini, sinon initialisez-le comme un objet vide
+  let etats = loadLocalStorage() || {};
+  console.log("etats in updateCounters:", etats);
+  const etatsCount = Object.values(etats).reduce((count, etat) => {
+    count[etat] = (count[etat] || 0) + 1;
+    return count;
+  }, {});
+
+  conformeCounter.textContent = etatsCount["conforme"] || 0;
+  enCoursCounter.textContent = etatsCount["en cours de déploiement"] || 0;
+  nonConformeCounter.textContent = etatsCount["non conforme"] || 0;
+  nonApplicableCounter.textContent = etatsCount["non applicable"] || 0;
+  scoreConformite.textContent = etatsCount["score de conformité"] || 0;
+
+  const totalCritereCount = data.criteres.length;
+  const definedCritereCount = Object.values(etats).filter(
+    (etat) => etat !== "a definir"
+  ).length;
+  const aDefinirCount = totalCritereCount - definedCritereCount;
+  aDefinirCounter.textContent = aDefinirCount;
+
+  const scoreConformiteCount =
+    (etatsCount["conforme"] || 0) /
+    (totalCritereCount - (etatsCount["non applicable"] || 0));
+  scoreConformite.textContent = scoreConformiteCount.toFixed(2) * 100 + "%";
+
+  return scoreConformite.textContent;
+}
+
 
     function updateTable() {
       tbody.innerHTML = "";
@@ -109,7 +120,6 @@ xhr.onload = function () {
           updateCounters();
           saveTemp(
             select.critereId,
-            //document.querySelector("#test").name.replace(/select-/g, ""),
             etats[critere.id]
           );
         });
@@ -132,7 +142,8 @@ xhr.onload = function () {
     }
 
     // Créer les en-têtes du tableau
-    tabVal = loadLocalStorage();
+    etats = loadLocalStorage();
+    
     const headerRow = document.createElement("tr");
     const thTheme = document.createElement("th");
     thTheme.className = "column1";
@@ -189,7 +200,7 @@ xhr.onload = function () {
         const optionEl = document.createElement("option");
         optionEl.value = option;
         optionEl.textContent = option;
-        if (optionEl.value == etats[critere.id]) optionEl.selected = true;
+        optionEl.selected = true;
         select.appendChild(optionEl);
       });
       select.value = tabVal==null?"a definir":tabVal[critere.id] ;
@@ -200,7 +211,6 @@ xhr.onload = function () {
         updateTable();
         saveTemp(
           select.critereId,
-          //document.querySelector("#test").name.replace(/select-/g, ""),
           e.target.value
         );
       });
@@ -224,19 +234,20 @@ xhr.onload = function () {
       .addEventListener("click", exportToPDF);
 
     function exportToPDF() {
-      const nomDuSite = document.getElementById("url").value;
-      const pdf = new window.jspdf.jsPDF();
+      const nomDuSite = document.getElementById("url").value ;
+      const pdf = new window.jspdf.jsPDF(); 
 
       pdf.text(
         "Liste des critères d'écoconception de services numériques",
         20,
-        10
+        20
       );
-
+      
       // Configure le titre
       pdf.setFontSize(16);
       const titre = `Évaluation de Conformité - ${nomDuSite}`;
       console.log("Titre:", titre);
+      pdf.text(titre, 20, 10);
 
       //recalculer score
       const scoreConformite = updateCounters();
@@ -245,11 +256,11 @@ xhr.onload = function () {
       const currentDate = new Date();
       const formattedDate = currentDate.toLocaleDateString("fr-FR");
       const dateText = `Date : ${formattedDate}`;
-      pdf.text(dateText, 20, 20);
+      pdf.text(dateText, 20, 30);
       pdf.text(
         `Score de Conformité : ${scoreConformite}`,
         pdf.internal.pageSize.width - 80,
-        20
+        30
       );
 
       // Récupère les données du tableau
@@ -267,13 +278,14 @@ xhr.onload = function () {
 
         tableData.push([theme, critere, etat]);
       });
+      
 
       // Configure les colonnes du tableau
       const columns = ["Thème", "Critère", "État"];
 
       // Ajoute le tableau avec les données
       pdf.autoTable({
-        startY: 30,
+        startY: 40,
         head: [columns],
         body: tableData,
       });
@@ -300,3 +312,27 @@ xhr.onload = function () {
 };
 
 xhr.send();
+
+
+
+
+
+
+
+
+document.getElementById("submitUrl").addEventListener("click", function() {
+  const enteredUrl = document.getElementById("url").value;
+
+  // Save the entered URL to localStorage
+  localStorage.setItem("enteredUrl", enteredUrl);
+
+  // Redirect to the section
+  window.location.hash = "#table-container";
+
+  // Add the 'hidden' class to the header
+  document.getElementById("main-header").classList.add("hidden");
+
+  // Optionally, you can update the displayed link immediately
+  // For example, you might display it in a div with an id "displayedUrl"
+  document.getElementById("displayedUrl").textContent = enteredUrl;
+});
